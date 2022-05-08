5e2f50987222561f37e7fe8dee80748d0372d79a
Abdulmalik-Dev <abdulmalikkld325@gmail.com>
2022-05-08 22:09:33 +0300

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Cash
-let spans = document.querySelectorAll("#scrolling-down span"),
-  count = 0;
-
-window.onscroll = function () {
-  if (window.scrollY >= spans[0].parentElement.offsetTop - 250) {
-  }
-};
