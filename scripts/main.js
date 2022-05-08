50e051307767da4610bd79232d7be133dfae11f4
Abdulmalik-Dev <abdulmalikkld325@gmail.com>
2022-05-08 21:50:33 +0300

    Add The Main Files

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Cash
+let spans = document.querySelectorAll("#scrolling-down span"),
+  count = 0;
+
+window.onscroll = function () {
+  if (window.scrollY >= spans[0].parentElement.offsetTop - 250) {
+  }
+};
